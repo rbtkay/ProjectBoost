edda0f6186a1a4d52e6e56f5641cb7cfaeeb34b2
robertkhayat <robert.g.khayat@gmail.com>
2018-12-10 17:19:58 +0200

    AudioSource + FreezePosition + SerializedFields + Tags

--- Assets/RocketScript.cs
+++ Assets/RocketScript.cs
@@ -5,34 +5,75 @@ using UnityEngine;
 
 public class RocketScript : MonoBehaviour
 {
+    [SerializeField] float rcsThrust;
+    [SerializeField] float mainThrust;
+
     Rigidbody shipBody;
+    AudioSource engineSound;
 
     // Use this for initialization
     void Start()
     {
         shipBody = GetComponent<Rigidbody>();
+        engineSound = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            shipBody.AddRelativeForce(Vector3.up);
+            shipBody.AddRelativeForce(Vector3.up * mainThrust);
+
+            if (!engineSound.isPlaying)
+            {
+                engineSound.Play();
+            }
         }
+        else
+        {
+            engineSound.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        shipBody.freezeRotation = true;
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        shipBody.freezeRotation = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                print("it's alright");
+                break;
+            case "Fuel":
+			print("Fuelling");
+                break;
+            default:
+                print("Dead");
+                break;
         }
     }
 }
