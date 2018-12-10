b8608cb7112c93f05fe72bfb7db59fc9ff7a65a1
robertkhayat <robert.g.khayat@gmail.com>
2018-12-10 14:41:38 +0200

    rocketScript

---
+++ Assets/RocketScript.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour
+{
+    Rigidbody shipBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        shipBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            shipBody.AddRelativeForce(Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
