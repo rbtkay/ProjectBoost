c04b519d06c4580681c2404baf7a2b3767a59b16
robertkhayat <robert.g.khayat@gmail.com>
2019-01-30 17:38:44 +0200

    wrap up Commit

---
+++ Assets/Script/OscillatorScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillatorScript : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
+    [Range(0, 1)] [SerializeField] float movementFactor; // 0 not moved, 1 for fully moved
+
+    // Use this for initialization
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon)
+            return;
+
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2 + 0.5f;
+
+        Vector3 offset = movementFactor * movementVector;
+        transform.position = startingPosition + offset;
+    }
+}
--- Assets/Script/RocketScript.cs
+++ Assets/Script/RocketScript.cs
@@ -6,45 +6,85 @@ using UnityEngine.SceneManagement;
 
 public class RocketScript : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 2f;
     [SerializeField] float rcsThrust;
     [SerializeField] float mainThrust;
 
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explosion;
+    [SerializeField] AudioClip finish;
+
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem explosionParticles;
+    [SerializeField] ParticleSystem finishParticles;
+
+
+    // public GameObject particleSystem;
     Rigidbody shipBody;
-    AudioSource engineSound;
+    AudioSource audioSource;
 
     enum State { Alive, Dying, Transcending };
     State rocketState = State.Alive;
+
+    bool isCollisionEnabled = true;
 
     // Use this for initialization
     void Start()
     {
         shipBody = GetComponent<Rigidbody>();
-        engineSound = GetComponent<AudioSource>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if (rocketState == State.Alive)
+        {
+            Thrust();
+            Rotate();
+        }
+
+        if (Debug.isDebugBuild)
+        {
+            DebugKeys();
+        }
     }
 
+    private void DebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            isCollisionEnabled = !isCollisionEnabled;
+        }
+    }
 
     private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            shipBody.AddRelativeForce(Vector3.up * mainThrust);
-
-            if (!engineSound.isPlaying)
-            {
-                engineSound.Play();
-            }
+            ApplyThrust();
         }
         else
         {
-            engineSound.Stop();
+            audioSource.Stop();
+            mainEngineParticles.Stop();
         }
+    }
+
+    private void ApplyThrust()
+    {
+        shipBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
     }
 
     private void Rotate()
@@ -67,6 +107,9 @@ public class RocketScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        if (rocketState != State.Alive || !isCollisionEnabled)
+            return;
+
         switch (other.gameObject.tag)
         {
             case "Friendly":
@@ -76,21 +119,47 @@ public class RocketScript : MonoBehaviour
                 print("Fuelling");
                 break;
             case "Finish":
-                rocketState = State.Transcending;
-                Invoke("LoadNextScene", 2f);
+                StartFinishSequence();
                 break;
             default:
-                print("Dead");
+                StartDeathSequence();
+                // Destroy(gameObject);
+                // GameObject particles = GameObject.Instantiate(particleSystem, transform.position, Quaternion.identity);
                 break;
         }
     }
 
-    public void LoadNextScene()
+    private void StartDeathSequence()
     {
-        SceneManager.LoadScene(1);
+        rocketState = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(explosion);
+        explosionParticles.Play();
+        Invoke("ReloadGame", 2f);
     }
 
-    public void ReloadLevel(){
-        // if(SceneManager.GetActiveScene
+    private void StartFinishSequence()
+    {
+        rocketState = State.Transcending;
+        audioSource.Stop();
+        audioSource.PlayOneShot(finish);
+        finishParticles.Play();
+        Invoke("LoadNextScene", levelLoadDelay);
+    }
+
+    public void LoadNextScene()
+    {
+        if ((SceneManager.GetActiveScene().buildIndex + 1) == SceneManager.sceneCountInBuildSettings)
+            Debug.Log("Game Finished!!");
+        else
+        {
+            Debug.Log("Go the Next Level");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
